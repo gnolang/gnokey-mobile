d336b65e5489ca0cf413371bc6464bb4988df02e
Iuri Pereira <689440+iuricmp@users.noreply.github.com>
2024-10-17 14:23:54 +0100

    feat: dynamic return to callback

--- mobile/app/(app)/tosign/index.tsx
+++ mobile/app/(app)/tosign/index.tsx
@@ -3,7 +3,7 @@ import Button from "@/components/button";
 import VaultListItem from "@/components/list/vault-list/VaultListItem";
 import Spacer from "@/components/spacer";
 import Text from "@/components/text";
-import { selectClientName, selectBech32Address, selectTxInput, signTx, useAppDispatch, useAppSelector, reasonSelector } from "@/redux";
+import { selectClientName, selectBech32Address, selectTxInput, signTx, useAppDispatch, useAppSelector, reasonSelector, selectCallback } from "@/redux";
 import { KeyInfo, useGnoNativeContext } from "@gnolang/gnonative";
 import { router, useNavigation } from "expo-router";
 import { useEffect, useState } from "react";
@@ -17,11 +17,13 @@ export default function Page() {
     const dispatch = useAppDispatch();
     const { gnonative } = useGnoNativeContext();
     const navigation = useNavigation();
-    const txInput = useAppSelector(selectTxInput);
-    const bech32Address = useAppSelector(selectBech32Address);
     const [accountName, setAccountName] = useState<string | undefined>(undefined);
     const clientName = useAppSelector(selectClientName);
+    
     const reason = useAppSelector(reasonSelector);
+    const bech32Address = useAppSelector(selectBech32Address);
+    const txInput = useAppSelector(selectTxInput);
+    const callback = useAppSelector(selectCallback);
 
     console.log('txInput', txInput);
     console.log('bech32Address', bech32Address);
@@ -30,8 +32,8 @@ export default function Page() {
 
     useEffect(() => {
         (async () => {
-        const accountNameStr = await gnonative.qEval("gno.land/r/demo/users", `GetUserByAddress("${bech32Address}").Name`);
-        setAccountName(accountNameStr);
+            const accountNameStr = await gnonative.qEval("gno.land/r/demo/users", `GetUserByAddress("${bech32Address}").Name`);
+            setAccountName(accountNameStr);
         })();
     }, [bech32Address]);
 
@@ -61,7 +63,8 @@ export default function Page() {
 
         const signedTx = await dispatch(signTx({ keyInfo })).unwrap();
 
-        Linking.openURL('tech.berty.dsocial://post?tx=' + encodeURIComponent(signedTx.signedTxJson));
+        const path = callback ? callback : 'tech.berty.dsocial://post';
+        Linking.openURL(`${path}?tx=${encodeURIComponent(signedTx.signedTxJson)}`);
 
         router.push("/home")
     }
@@ -73,6 +76,11 @@ export default function Page() {
                     <Text.Title>{clientName} is requiring permission to {reason}.</Text.Title>
                     <Spacer space={16} />
                     <Text.Body>In the next version of this app you'll get more details about the transaction dSocial is trying to execute.</Text.Body>
+
+                    <Spacer space={16} />
+                    <Text.Body>reason: {reason}</Text.Body>
+                    <Text.Body>callback: {callback}</Text.Body>
+                    <Text.Body>client_name: {clientName}</Text.Body>
 
                     <Spacer space={16} />
                     <Text.Title>For now, please select a key {accountName} to sign the transaction</Text.Title>

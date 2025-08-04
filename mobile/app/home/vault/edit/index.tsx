0cbe2ab89f5a7f06d78e66874f5ab28b509d8343
Iuri Pereira <689440+iuricmp@users.noreply.github.com>
2025-08-04 20:09:12 -0300

    success screen

--- mobile/app/home/vault/edit/index.tsx
+++ mobile/app/home/vault/edit/index.tsx
@@ -37,7 +37,7 @@ const Page = () => {
     await dispatch(deleteVault({ vault })).unwrap()
     await dispatch(fetchVaults()).unwrap()
     setShowDeleteModal(false)
-    router.replace({ pathname: '/home/vault/add/delete-success', params: { keyName: vaultName } })
+    router.replace({ pathname: '/home/vault/edit/remove-success', params: { keyName: vaultName } })
     navigation.goBack()
   }
 
@@ -47,7 +47,7 @@ const Page = () => {
     await dispatch(updateVault(params)).unwrap()
     await dispatch(fetchVaults()).unwrap()
     router.replace({
-      pathname: '/home/vault/add/new-vault-success',
+      pathname: '/home/vault/edit/edit-success',
       params: { keyName: vaultName }
     })
   }
@@ -74,7 +74,7 @@ const Page = () => {
       <OnboardingLayout
         footer={
           <Button onPress={onUpdateAccount} color="primary">
-            Update Account
+            Update Account 2
           </Button>
         }
       >
